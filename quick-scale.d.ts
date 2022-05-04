87c260593bcba17205d17499dda45788b6d590d8
DanielJDufour <daniel.j.dufour@gmail.com>
2022-05-04 19:19:10 -0400

    added NoRangeValueStrategy type

--- quick-scale.d.ts
+++ quick-scale.d.ts
@@ -1,10 +1,12 @@
+export type NoRangeValueStrategy = "highest" | "middle" | "lowest";
+
 export declare function createScaleFunction(
   [old_min, old_max]: [number, number],
   [new_min, new_max]: [number, number],
   options?: {
     flip?: boolean;
     no_range_value?: number;
-    no_range_value_strategy?: "highest" | "middle" | "lowest";
+    no_range_value_strategy?: NoRangeValueStrategy;
     round?: boolean;
   }
 ): (value: number) => number;
